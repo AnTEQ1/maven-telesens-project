3152691395884207ef719dbcfac37fbd0a4ad55f
AnTEQ1 <an.andrey.90@gmail.com>
2021-01-21 19:42:27 +0200

    ДЗ. Работа с файлами. Задание 3

--- src/main/java/com/academy/telesens/lesson11/excel/ExcelWriteDemo.java
+++ src/main/java/com/academy/telesens/lesson11/excel/ExcelWriteDemo.java
@@ -10,16 +10,26 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 
-public class ExcelReaderDemo {
+public class ExcelWriteDemo {
     public static void main(String[] args) {
         Workbook workbook = new XSSFWorkbook();
         Sheet sheet = workbook.createSheet();
         Row row = sheet.createRow(0);
         Cell cell1 = row.createCell(0);
         Cell cell2 = row.createCell(1);
+        Cell cell3 = row.createCell(2);
 
         cell1.setCellValue("Hello");
         cell2.setCellValue("World");
+        cell3.setCellValue(25.0);
+
+        Row row2 = sheet.createRow(1);
+        Cell cell4 = row2.createCell(0);
+        Cell cell5 = row2.createCell(1);
+        Cell cell6 = row2.createCell(2);
+        cell4.setCellValue("Last Row Data");
+        cell5.setCellValue("!");
+        cell6.setCellValue(36.0);
 
         //Запись на диск
         String path = "D:/Тестирование/TelesenseAcademy/demo.xlsx";
