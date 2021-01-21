0cd36ed3b28e43573ff4bc2a2b40328d24ac9ede
AnTEQ1 <an.andrey.90@gmail.com>
2021-01-21 21:04:14 +0200

    Lesson12

--- src/main/java/com/academy/telesens/lesson12/MySQLDemo.java
+++ src/main/java/com/academy/telesens/lesson12/MySQLDemo.java
@@ -1,18 +1,37 @@
 package com.academy.telesens.lesson12;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.Statement;
+import java.sql.*;
 
 public class MySQLDemo {
     public static void main(String[] args) {
         String url = "jdbc:mysql://localhost:3306/operator?user=root&password=root";
-        String sqlInsert = "Insert into subscriber values (5, 'demo', 'demo', 'm', 23 )";
-        try {
+        String sqlInsert = "Insert into subscriber (first_name, last_name, gender, age) values ('demo', 'demo', 'm', 23 )";
+        String sqlInsertTemplate = "Insert into subscriber (first_name, last_name, gender, age) values (?, ?, ?, ? )";
+        try (Connection conn = DriverManager.getConnection(url)) {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            Connection conn = DriverManager.getConnection(url);
             Statement statement = conn.createStatement();
             statement.executeUpdate(sqlInsert);
+
+            PreparedStatement preparedStatement = conn.prepareStatement(sqlInsertTemplate);
+            preparedStatement.setString(1,"demo_firstname_2");
+            preparedStatement.setString(2,"demo_lastname_2");
+            preparedStatement.setString(3,"f");
+            preparedStatement.setInt(4,26);
+            preparedStatement.executeUpdate();
+
+            // Полное чтение таблицы subscriber
+            Statement readStatement = conn.createStatement();
+            ResultSet resultSet = readStatement.executeQuery("Select * from subscriber");
+
+            while (resultSet.next()) {
+                int id = resultSet.getInt("subscriber_id");
+                String firstName = resultSet.getString("first_name");
+                String lastName = resultSet.getString("last_name");
+                String gender = resultSet.getString("gender");
+                int age = resultSet.getInt("age");
+
+                System.out.println(String.format("%d | %s | %s | %s | %d", id, firstName, lastName, gender, age));
+            }
         } catch (Exception e) {
             System.out.println(e);
         }
